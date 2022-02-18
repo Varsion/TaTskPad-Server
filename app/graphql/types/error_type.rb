b9bff641af60d1fcd4eba8b275dfd8e92068046a
Varsion <JianhuaL12@outlook.com>
2022-02-18 23:40:29 +0800

    update error format

--- app/graphql/types/error_type.rb
+++ app/graphql/types/error_type.rb
@@ -1,6 +1,21 @@
 module Types
   class ErrorType < Types::Base::Object
-    field :path, String, null: false
     field :message, String, null: false
+    field :path, [String], null: true
+
+    class << self
+      def errors_of(model)
+        model.errors.collect do |attribute, message|
+          format(attribute, message)
+        end
+      end
+
+      def format(attribute, message)
+        {
+          path: [attribute],
+          message: message
+        }
+      end
+    end
   end
 end
