8ca77d5c71a6999fdbc04d6f040e222d30dc2e89
Varsion <JianhuaL12@outlook.com>
2022-02-19 15:42:24 +0800

    merger error_type and model_error, fix the model_error to Rails.6.1

--- app/graphql/types/base/model_error.rb
+++ app/graphql/types/base/model_error.rb
@@ -2,20 +2,20 @@ module Types
     module Base
       class ModelError < Types::Base::Object
         description "ActiveModel::Errors"
-  
+
+        field :attribute, String, null: true
         field :message, String, null: false
-        field :path, [String], null: true
-  
+
         class << self
           def errors_of(model)
-            model.errors.collect do |attribute, message|
-              format(attribute, message)
+            model.errors.collect do |error|
+              format(error.attribute, error.message)
             end
           end
   
           def format(attribute, message)
             {
-              path: [attribute],
+              attribute: attribute.to_s,
               message: message
             }
           end
