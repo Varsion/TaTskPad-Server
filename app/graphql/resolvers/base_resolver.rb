d1b42aa76422a548c24ec22c37a7c1d2729f1faa
Varsion <JianhuaL12@outlook.com>
2022-02-27 16:11:00 +0800

    improve query func

--- app/graphql/resolvers/base_resolver.rb
+++ app/graphql/resolvers/base_resolver.rb
@@ -1,5 +1,16 @@
 module Resolvers
-  class BaseResolver < GraphQL::Schema::Resolver 
+  class BaseResolver < GraphQL::Schema::Resolver
+    protected
+
+    def authenticate_user!
+      raise GraphQL::ExecutionError, "unauthenticated" unless current_account
+    end
+
+    private
+
+    def current_account
+      @current_account ||= context[:account]
+    end
   end
 end
   
