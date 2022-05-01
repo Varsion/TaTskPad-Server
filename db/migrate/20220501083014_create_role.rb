1ecf55c0ebe735dc2a7dadfa203e3b6b52425fe2
Varsion <JianhuaL12@outlook.com>
2022-05-01 18:26:50 +0800

    feat: update belongs to type

--- db/migrate/20220501083014_create_role.rb
+++ db/migrate/20220501083014_create_role.rb
@@ -2,7 +2,7 @@ class CreateRole < ActiveRecord::Migration[6.1]
   def change
     create_table :roles, id: :uuid do |t|
       t.string :name, null: false
-      t.belongs_to :organization
+      t.belongs_to :organization, type: :uuid, null: false
       t.string :description
       t.jsonb :permissions, null: false, default: {}
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -171,7 +171,7 @@ ActiveRecord::Schema.define(version: 2022_05_01_083014) do
 
   create_table "roles", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "name", null: false
-    t.bigint "organization_id"
+    t.uuid "organization_id", null: false
     t.string "description"
     t.jsonb "permissions", default: {}, null: false
     t.datetime "created_at", precision: 6, null: false
