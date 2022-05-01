32aff9ae9cb414148bbfb536385769bb102121f8
Varsion <JianhuaL12@outlook.com>
2022-05-01 10:53:24 +0800

    fix: fix the nil error ( not cover now

--- app/graphql/resolvers/project_resolver.rb
+++ app/graphql/resolvers/project_resolver.rb
@@ -6,6 +6,7 @@ module Resolvers
     def resolve(project_id:)
       authenticate_user!
       project = Project.find_by(id: project_id)
+      return nil if project.nil?
       project.organization.is_member?(current_account) ? project : nil
     end
   end
